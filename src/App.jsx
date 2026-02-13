36a99935b51f3e52a84b6ca906a40c7f48431378
MihaiNeagu9 <mihaineagu2001@yahoo.com>
2026-02-13 13:52:21 +0200

    feat: add quiz and answers table

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,22 @@ import Header from "./components/Header.jsx";
 
 function App() {
     return (
+        <>
         <Header />
+
+        <div id="quiz">
+            <div id="question">
+                <h2>Question ?</h2>
+            </div>
+
+            <ol id="answers">
+                <li className="answer"><button>Response 1</button></li>
+                <li className="answer"><button>Response 2</button></li>
+                <li className="answer"><button>Response 3</button></li>
+                <li className="answer"><button>Response 4</button></li>
+            </ol>
+        </div>
+        </>
     );
 }
 
