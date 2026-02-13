0f9db55cfe7f924c251e775670debce5c936f00d
MihaiNeagu9 <mihaineagu2001@yahoo.com>
2026-02-13 13:36:22 +0200

    feat: add header section to main component App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
-function App() {}
+import quizLogo from "./assets/quiz-logo.png";
+
+function App() {
+    return (
+        <header>
+            <img src={quizLogo} alt="quiz logo" />
+            <h1>Quiz</h1>
+        </header>
+    );
+}
 
 export default App;
