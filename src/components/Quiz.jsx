def8fd5f5f84af7b824f8d0c7a84d0b2baa056ae
MihaiNeagu9 <mihaineagu2001@yahoo.com>
2026-02-16 12:21:11 +0200

    feat: switch questions once choose an answer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import { QUESTION_DATA } from "../data-question.js";
 
 export default function Quiz() {
-    const currentQuestion = QUESTION_DATA[0];
+    const [questionIndex, setQuestionIndex] = useState(0);
+
+    const currentQuestion = QUESTION_DATA[questionIndex];
+
+    function handleAnswerClick() {
+        if (questionIndex + 1 === QUESTION_DATA.length) {
+            setQuestionIndex(0);
+        } else {
+            setQuestionIndex(questionIndex + 1);
+        }
+    }
 
     return (
     <div id="quiz">
@@ -11,7 +22,7 @@ export default function Quiz() {
 
         <ol id="answers">
             {currentQuestion.answers.map((answer, indexAnswer) => {
-                return(<li key={indexAnswer} className="answer"><button>{answer}</button></li>)
+                return(<li key={indexAnswer} className="answer"><button onClick={handleAnswerClick}>{answer}</button></li>)
             })}
         </ol>
     </div>
--- src/data-question.js
+++ src/data-question.js
@@ -1,3 +1,6 @@
 export const QUESTION_DATA = [
     {question: "question 1", answers: ["answer 1", "answer 2", "answer 3", "answer 4"]},
+    {question: "question 2", answers: ["answer 1", "answer 2", "answer 3", "answer 4"]},
+    {question: "question 3", answers: ["answer 1", "answer 2", "answer 3", "answer 4"]},
+    {question: "question 4", answers: ["answer 1", "answer 2", "answer 3", "answer 4"]},
 ];
