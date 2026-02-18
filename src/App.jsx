6d3e048ee4f6119ecc8a39b615bae480239cd2c2
MihaiNeagu9 <mihaineagu2001@yahoo.com>
2026-02-18 13:52:58 +0200

    feat: manage quiz answers in App and switch to Summary on completion (no UI updated)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
+import { QUESTION_DATA } from "./data-question.js";
+
 import Header from "./components/Header.jsx";
 import Quiz from "./components/Quiz.jsx";
 import Summary from "./components/Summary.jsx";
 
 function App() {
+    const [userAnswers, setUserAnswers] = useState([]);
+
+    const quizIsComplete = userAnswers.length === QUESTION_DATA.length;
+
     return (
         <>
         <Header />
 
-        <Summary />
-
-        <Quiz />
+        {quizIsComplete ? <Summary /> : <Quiz />}
         </>
     );
 }
